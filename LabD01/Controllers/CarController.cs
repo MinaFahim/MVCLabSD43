d56c141b873c1757eb18f4583e3c8f3947872f85
MinaFahim <minagerges655@gmail.com>
2023-03-12 22:48:07 +0200

    Add project files.

---
+++ LabD01/Controllers/CarController.cs
@@ -0,0 +1,21 @@
+﻿using LabD01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabD01.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            var cars = Car.GetCars();
+            return View(cars);
+        } public IActionResult GetDetial(string model,string type)
+        {
+            var cars = Car.GetCars().First(e=>e.Model == model);
+            var cardata = new { type, cars };
+            return View(cardata);  
+           
+            
+        }
+    }
+}
